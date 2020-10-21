62b950acd71b959ff0a737ce7049f00adb656f6a
danielbbr <72083195+danielbbr@users.noreply.github.com>
2020-10-21 12:18:50 -0300

    Add files via upload

--- ebook.py
+++ ebook.py
@@ -1,25 +1,37 @@
 from kivy.uix.boxlayout import BoxLayout
 from kivy.app import App
 
-with open('1984.txt', 'r', errors='ignore') as livro:
+
+with open('1984.txt', 'r', errors='ignore', encoding='latin-1') as livro:
     label = livro.read()
 
 
 class Controller(BoxLayout):
     cont = 3000
-    with open('1984.txt', 'r', errors = 'ignore') as livro:
-        label = livro.read()[0:cont]
+    label = label[0:cont]
+
     m = 0
+
     def next_page(self):
         self.m = self.m + 1
         print(self.m)
         self.label_wid.text = label[self.cont*self.m:self.cont*(self.m+1)]
+        self.pagenum.hint_text = str(self.m + 1)
+        self.pagenum.text = str(self.m + 1)
 
     def last_page(self):
         self.m = self.m - 1
         print(self.m)
         self.label_wid.text = label[self.cont*self.m:self.cont*(self.m+1)]
+        self.pagenum.hint_text = str(self.m + 1)
+        self.pagenum.text = str(self.m + 1)
 
+    def page(self):
+        self.m = int(self.pagenum.text) - 1
+        self.label_wid.text = label[self.cont * self.m:self.cont * (self.m + 1)]
+        print(self.m)
+        self.pagenum.hint_text = str(self.m + 1)
+        self.pagenum.text = str(self.m + 1)
 
 class ControllerApp(App):
 
